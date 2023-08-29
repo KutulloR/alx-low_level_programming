3d6c3ad2d69ec80000d73a40b1a5ce24909ab199
KutulloR <rantlokakutullo@gmail.com>
2023-08-29 01:12:01 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
-    const listint_t *current = h;
+	size_t num = 0;
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        num++;
-        current = current->next;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
 
-    return (num);
+	return (num);
 }
